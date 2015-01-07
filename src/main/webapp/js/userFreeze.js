b8ccc7242b490ecb4fd64a4692c8ca17f963adc5
hanchen123 <chenhan728@gmail.com>
2015-01-07 16:06:55 -0500

    av

--- src/main/webapp/js/userFreeze.js
+++ src/main/webapp/js/userFreeze.js
@@ -3,9 +3,11 @@ function validateLogin()
 {
 	var cid=document.getElementById("username");
 	var cacc=document.getElementById("account");
+	var cac=document.getElementById("action");
 	
 	var cidTD=document.getElementById("usernameTD");
 	var caccTD=document.getElementById("accountTD");
+	var cacTD=document.getElementById("actionTD");
 
 	if(cacc.value=="")
 	{
@@ -15,7 +17,7 @@ function validateLogin()
 
 	if(!(cacc.value=="saving" || cacc.value=="checking" || cacc.value=="credit card"))
 	{
-		caccTD.innerHTML="<font style='color:red; font-size:18px'>*Please enter valid account (checking or saving)</font>";
+		caccTD.innerHTML="<font style='color:red; font-size:18px'>*Please enter valid account (checking, saving or credit card)</font>";
 		return false;
 	}
 
@@ -24,7 +26,18 @@ function validateLogin()
 		cidTD.innerHTML="<font style='color:red; font-size:18px'>*Please enter username</font>";
 		return false;
 	}
+	if(cac.value=="")
+	{
+		cacTD.innerHTML="<font style='color:red; font-size:18px'>*Please enter method</font>";
+		return false;
+	}
 
+	if(!(cac.value=="freeze" || cac.value=="available"))
+	{
+		cacTD.innerHTML="<font style='color:red; font-size:18px'>*Please enter valid method (freeze or available)</font>";
+		return false;
+	}
+	
 	return true;
 }
 
